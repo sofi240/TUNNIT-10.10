ec1b6d285f6c5e6b8394008625861150e02342fa
SophiaSillaots <sillaotssophia@perg.ee>
2024-10-10 11:20:03 +0300

    Tunnitöö lahendatud, samuti ka praktilinetöö

--- praktiline_too_10.10.py
+++ praktiline_too_10.10.py
@@ -1,11 +1,21 @@
 """Matemaatilised tehted"""
-
+import math
 
 # küsime kasutajalt ujuvkomaarvu kujul kolmnurga kaatetid a ja b
 print("sisesta kolmnurga kaatetid a ja b:")
 # koosta muutuja a, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
-a = int(input("sisesta oma a:"))
+a = float(input("sisesta oma a:"))
 # koosta muutuja b, lisa võimalus kasutajal sisestada arv, muuda see ujuvkomaarvuks
-b = int(input("sisesta oma b:"))
+b = float(input("sisesta oma b:"))
 # meie ülesandeks on leida hüpoteenus c, kolmnurga ümbermõõt ja pindala NB(vastused peavad olema ümardatud sajandikeni)
-c = (a + b)
+c = math.sqrt(a ** 2 + b ** 2)
+p = (a + b + c)
+s = (a * b) / 2
+
+print(f"Kolmnurga hüpotenuus c={round(c,2)}")
+print(f"Kolmnurga ümbermõõt p={round(p,2)}")
+print(f"Kolmnurga pindala s={round(s,2)}")
+
+
+
+
